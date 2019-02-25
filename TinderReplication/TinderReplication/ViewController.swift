bfd4a9a2ac814b0168481eb5ee6b94e18adc561e
Subhodip <differentialthinking@gmail.com>
2019-02-25 16:10:03 +0530

    First Commit.

---
+++ TinderReplication/TinderReplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TinderReplication
+//
+//  Created by Banerjee,Subhodip on 25/02/19.
+//  Copyright © 2019 Banerjee,Subhodip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
